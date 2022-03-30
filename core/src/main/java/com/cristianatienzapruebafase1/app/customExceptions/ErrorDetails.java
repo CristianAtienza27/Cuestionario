3e5f148aa6f60db17478a0eef8e2d94ec8d8050e
CristianAtienza27 <c.atienza@cesjuanpablosegundocadiz.es>
2022-03-30 12:38:07 +0200

    Practica10: ok

---
+++ core/src/main/java/com/cristianatienzapruebafase1/app/customExceptions/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.cristianatienzapruebafase1.app.customExceptions;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+    
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
